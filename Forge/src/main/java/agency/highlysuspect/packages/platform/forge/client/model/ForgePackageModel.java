7442cfe8298a77b04621dac804b97932723dbd1a
quat1024 <quat1024@users.noreply.github.com>
2022-10-09 01:54:30 -0400

    item models on forge except the retexturing isnt working

--- Forge/src/main/java/agency/highlysuspect/packages/platform/forge/client/model/ForgePackageModel.java
+++ Forge/src/main/java/agency/highlysuspect/packages/platform/forge/client/model/ForgePackageModel.java
@@ -11,6 +11,7 @@ import com.google.gson.JsonObject;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.ItemOverrides;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
@@ -24,8 +25,12 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.BakedModelWrapper;
 import net.minecraftforge.client.model.IModelConfiguration;
@@ -68,6 +73,7 @@ public class ForgePackageModel implements IModelGeometry<ForgePackageModel> {
 		}
 		
 		private final PackageModelBakery<List<BakedQuad>> factory;
+		private final WeirdItemOverrideThing itemOverride = new WeirdItemOverrideThing();
 		
 		@Override
 		public BakedModel handlePerspective(ItemTransforms.TransformType cameraTransformType, PoseStack poseStack) {
@@ -110,6 +116,44 @@ public class ForgePackageModel implements IModelGeometry<ForgePackageModel> {
 			if(batg instanceof Level level && pos != null && style != null && !(style.innerBlock() instanceof PackageBlock)) {
 				return Minecraft.getInstance().getBlockRenderer().getBlockModelShaper().getTexture(style.innerBlock().defaultBlockState(), level, pos);
 			} else return super.getParticleIcon(data);
+		}
+		
+		@Override
+		public ItemOverrides getOverrides() {
+			return itemOverride;
+		}
+		
+		public class WeirdItemOverrideThing extends ItemOverrides {
+			private final PackageModelBakery<BakedModel> help = new PackageModelBakery.Caching<>(new PackageModelBakery<>() {
+				@Override
+				public BakedModel getBaseModel() {
+					return factory.getBaseModel();
+				}
+				
+				@Override
+				public BakedModel bake(@Nullable Object cacheKey, @Nullable DyeColor faceColor, @Nullable Block frameBlock, @Nullable Block innerBlock) {
+					List<BakedQuad> rehreh = factory.bake(cacheKey, faceColor, frameBlock, innerBlock);
+					return new BakedModelWrapper<>(getBaseModel()) {
+						@Override
+						public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, Random rand) {
+							return rehreh;
+						}
+						
+						@NotNull
+						@Override
+						public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, @NotNull Random rand, @NotNull IModelData extraData) {
+							return rehreh;
+						}
+					};
+				}
+			});
+			
+			@Nullable
+			@Override
+			public BakedModel resolve(BakedModel originalModel, ItemStack stack, @Nullable ClientLevel level, @Nullable LivingEntity player, int idk) {
+				PackageStyle style = PackageStyle.fromItemStack(stack);
+				return help.bake(style, style.color(), style.frameBlock(), style.innerBlock());
+			}
 		}
 	}
 	
