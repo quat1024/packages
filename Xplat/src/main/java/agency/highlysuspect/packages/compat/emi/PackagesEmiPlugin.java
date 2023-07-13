d6e9fb9ba86bbd87f0361549c94d3399c3f7e492
quat1024 <quat1024@users.noreply.github.com>
2023-07-13 04:03:43 -0400

    Pedantic whitespace commit
    Sorry about the fabric.mod.json splash-damage but I didn't know that
    file was using spaces :joy:

--- Xplat/src/main/java/agency/highlysuspect/packages/compat/emi/PackagesEmiPlugin.java
+++ Xplat/src/main/java/agency/highlysuspect/packages/compat/emi/PackagesEmiPlugin.java
@@ -29,109 +29,109 @@ import java.util.Random;
 
 @EmiEntrypoint
 public class PackagesEmiPlugin implements EmiPlugin {
-    public static final EmiRecipeCategory PACKAGE_MAKER_CATEGORY = new EmiRecipeCategory(
-            Packages.id("package_maker"),
-            EmiStack.of(PBlocks.PACKAGE_MAKER.get())
-    ) {
-        @Override
-        public Component getName() {
-            return Component.translatable("block.packages.package_maker");
-        }
-    };
-
-    @Override
-    public void register(EmiRegistry registry) {
-        registry.addCategory(PACKAGE_MAKER_CATEGORY);
-        registry.addWorkstation(PACKAGE_MAKER_CATEGORY, EmiStack.of(PBlocks.PACKAGE_MAKER.get()));
-        registry.addRecipe(new PackageRecipe());
-    }
-
-    static class PackageRecipe implements EmiRecipe {
-        private static final int uniq = new Random().nextInt();
-        private static final List<DyeColor> validDyes = new ArrayList<>();
-        private static final List<Block> validFrames = new ArrayList<>();
-        private static final List<Block> validInners = new ArrayList<>();
-
-        static {
-            for (DyeColor dyeColor : DyeColor.values()) {
-                if (PackageMakerBlockEntity.matchesDyeSlot(new ItemStack(DyeItem.byColor(dyeColor)))) {
-                    validDyes.add(dyeColor);
-                }
-            }
-            BuiltInRegistries.BLOCK.forEach(block -> {
-                ItemStack s = new ItemStack(block);
-                if (PackageMakerBlockEntity.matchesFrameSlot(s)) {
-                    validFrames.add(block);
-                }
-                if (PackageMakerBlockEntity.matchesInnerSlot(s)) {
-                    validInners.add(block);
-                }
-            });
-        }
-
-        @Override
-        public EmiRecipeCategory getCategory() {
-            return PACKAGE_MAKER_CATEGORY;
-        }
-
-        @Override
-        public @Nullable ResourceLocation getId() {
-            return Packages.id("/package_maker/package");
-        }
-
-        @Override
-        public List<EmiIngredient> getInputs() {
-            return List.of(
-                    EmiIngredient.of(validFrames.stream().map(EmiStack::of).toList()),
-                    EmiIngredient.of(validInners.stream().map(EmiStack::of).toList()),
-                    EmiIngredient.of(validDyes.stream().map(DyeItem::byColor).map(EmiStack::of).toList()),
-                    EmiStack.of(Items.COPPER_INGOT)
-            );
-        }
-
-        @Override
-        public List<EmiStack> getOutputs() {
-            return List.of(EmiStack.of(PItems.PACKAGE.get()));
-        }
-
-        @Override
-        public int getDisplayWidth() {
-            return 106;
-        }
-
-        @Override
-        public int getDisplayHeight() {
-            return 38;
-        }
-
-        @Override
-        public void addWidgets(WidgetHolder widgets) {
-            widgets.addGeneratedSlot(r -> getStack(r, 0), uniq, 0, 0);
-            widgets.addGeneratedSlot(r -> getStack(r, 1), uniq, 20, 0);
-            widgets.addGeneratedSlot(r -> getStack(r, 2), uniq, 0, 20);
-            widgets.addSlot(EmiStack.of(Items.COPPER_INGOT), 20, 20);
-
-            widgets.addTexture(EmiTexture.EMPTY_ARROW, 48, 10);
-
-            widgets.addGeneratedSlot(r -> getStack(r, 3), uniq, 80, 6).large(true).recipeContext(this);
-        }
-
-        @Override
-        public boolean supportsRecipeTree() {
-            return false;
-        }
-
-        private EmiStack getStack(Random r, int i) {
-            Block frame = validFrames.get(r.nextInt(validFrames.size()));
-            Block inner = validInners.get(r.nextInt(validInners.size()));
-            DyeColor dye = validDyes.get(r.nextInt(validDyes.size()));
-            EmiStack output = EmiStack.of(PItems.PACKAGE.get().createCustomizedStack(frame, inner, dye));
-            return new EmiStack[] {
-                    EmiStack.of(frame),
-                    EmiStack.of(inner),
-                    EmiStack.of(DyeItem.byColor(dye)),
-                    output
-            }[i];
-        }
-    }
+	public static final EmiRecipeCategory PACKAGE_MAKER_CATEGORY = new EmiRecipeCategory(
+		Packages.id("package_maker"),
+		EmiStack.of(PBlocks.PACKAGE_MAKER.get())
+	) {
+		@Override
+		public Component getName() {
+			return Component.translatable("block.packages.package_maker");
+		}
+	};
+	
+	@Override
+	public void register(EmiRegistry registry) {
+		registry.addCategory(PACKAGE_MAKER_CATEGORY);
+		registry.addWorkstation(PACKAGE_MAKER_CATEGORY, EmiStack.of(PBlocks.PACKAGE_MAKER.get()));
+		registry.addRecipe(new PackageRecipe());
+	}
+	
+	static class PackageRecipe implements EmiRecipe {
+		private static final int uniq = new Random().nextInt();
+		private static final List<DyeColor> validDyes = new ArrayList<>();
+		private static final List<Block> validFrames = new ArrayList<>();
+		private static final List<Block> validInners = new ArrayList<>();
+		
+		static {
+			for(DyeColor dyeColor : DyeColor.values()) {
+				if(PackageMakerBlockEntity.matchesDyeSlot(new ItemStack(DyeItem.byColor(dyeColor)))) {
+					validDyes.add(dyeColor);
+				}
+			}
+			BuiltInRegistries.BLOCK.forEach(block -> {
+				ItemStack s = new ItemStack(block);
+				if(PackageMakerBlockEntity.matchesFrameSlot(s)) {
+					validFrames.add(block);
+				}
+				if(PackageMakerBlockEntity.matchesInnerSlot(s)) {
+					validInners.add(block);
+				}
+			});
+		}
+		
+		@Override
+		public EmiRecipeCategory getCategory() {
+			return PACKAGE_MAKER_CATEGORY;
+		}
+		
+		@Override
+		public @Nullable ResourceLocation getId() {
+			return Packages.id("/package_maker/package");
+		}
+		
+		@Override
+		public List<EmiIngredient> getInputs() {
+			return List.of(
+				EmiIngredient.of(validFrames.stream().map(EmiStack::of).toList()),
+				EmiIngredient.of(validInners.stream().map(EmiStack::of).toList()),
+				EmiIngredient.of(validDyes.stream().map(DyeItem::byColor).map(EmiStack::of).toList()),
+				EmiStack.of(Items.COPPER_INGOT)
+			);
+		}
+		
+		@Override
+		public List<EmiStack> getOutputs() {
+			return List.of(EmiStack.of(PItems.PACKAGE.get()));
+		}
+		
+		@Override
+		public int getDisplayWidth() {
+			return 106;
+		}
+		
+		@Override
+		public int getDisplayHeight() {
+			return 38;
+		}
+		
+		@Override
+		public void addWidgets(WidgetHolder widgets) {
+			widgets.addGeneratedSlot(r -> getStack(r, 0), uniq, 0, 0);
+			widgets.addGeneratedSlot(r -> getStack(r, 1), uniq, 20, 0);
+			widgets.addGeneratedSlot(r -> getStack(r, 2), uniq, 0, 20);
+			widgets.addSlot(EmiStack.of(Items.COPPER_INGOT), 20, 20);
+			
+			widgets.addTexture(EmiTexture.EMPTY_ARROW, 48, 10);
+			
+			widgets.addGeneratedSlot(r -> getStack(r, 3), uniq, 80, 6).large(true).recipeContext(this);
+		}
+		
+		@Override
+		public boolean supportsRecipeTree() {
+			return false;
+		}
+		
+		private EmiStack getStack(Random r, int i) {
+			Block frame = validFrames.get(r.nextInt(validFrames.size()));
+			Block inner = validInners.get(r.nextInt(validInners.size()));
+			DyeColor dye = validDyes.get(r.nextInt(validDyes.size()));
+			EmiStack output = EmiStack.of(PItems.PACKAGE.get().createCustomizedStack(frame, inner, dye));
+			return new EmiStack[]{
+				EmiStack.of(frame),
+				EmiStack.of(inner),
+				EmiStack.of(DyeItem.byColor(dye)),
+				output
+			}[i];
+		}
+	}
 }
